235c1d3c67da3c14595b6aa7d7184dbacec9724e
dativus <sn.sable005@gmail.com>
2023-03-28 21:59:08 +0900

    Migrate the schema

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+
+  "gorm.io/gorm"
+  "gorm.io/driver/sqlite"
+
+  "github.com/ghsable/graphql-playground.go/graph/model"
+)
+
+type Product struct {
+  gorm.Model
+  Code  string
+  Price uint
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+
+  db, err := gorm.Open(sqlite.Open("dev.db"), &gorm.Config{})
+  if err != nil {
+    panic("failed to connect database")
+  }
+
+  // Migrate the schema
+  db.AutoMigrate(&model.NewTodo{})
+
+  // Create
+  //db.Create(&Product{Code: "D42", Price: 100})
+
+  fmt.Println("Finish")
 }
