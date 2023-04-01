b8e51781c1b76c2540b60082a1cf8164d6603dd3
dativus <sn.sable005@gmail.com>
2023-04-01 23:43:05 +0900

    go fmt migrate.go

--- migrate.go
+++ migrate.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-  "fmt"
-  "log"
+	"fmt"
+	"log"
 
-  "gorm.io/gorm"
-  "gorm.io/driver/sqlite"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type User struct {
-  gorm.Model
+	gorm.Model
 
-  Email string
-  Name string
-  IsActive bool
+	Email    string
+	Name     string
+	IsActive bool
 }
 
 func main() {
 
-  db := dbInit()
+	db := dbInit()
 
-  db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{})
 
-  insert(db)
+	insert(db)
 
-  fmt.Println("DONE.")
+	fmt.Println("DONE.")
 }
 
 func dbInit() *gorm.DB {
-  dsn := "dev.db"
-  db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
-  if err != nil {
-    panic("failed to connect database")
-  }
-  return db
+	dsn := "dev.db"
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
 }
 
 func insert(db *gorm.DB) {
 	users := []User{
 		{
-			Email:       "a@xxx.com",
-			Name:        "name-a",
-			IsActive:    true,
+			Email:    "a@xxx.com",
+			Name:     "name-a",
+			IsActive: true,
 		},
 		{
-			Email:       "b@xxx.com",
-			Name:        "name-b",
-			IsActive:    true,
+			Email:    "b@xxx.com",
+			Name:     "name-b",
+			IsActive: true,
 		},
 		{
-			Email:       "c@xxx.com",
-			Name:        "name-c",
-			IsActive:    true,
+			Email:    "c@xxx.com",
+			Name:     "name-c",
+			IsActive: true,
 		},
 		{
-			Email:       "d@xxx.com",
-			Name:        "name-d",
-			IsActive:    false,
+			Email:    "d@xxx.com",
+			Name:     "name-d",
+			IsActive: false,
 		},
 		{
-			Email:       "e@xxx.com",
-			Name:        "name-e",
-			IsActive:    false,
+			Email:    "e@xxx.com",
+			Name:     "name-e",
+			IsActive: false,
 		},
 	}
 	result := db.Create(&users)
