19951d92f705d011b788dd3f3d6acf9c5a746bf6
dativus <sn.sable005@gmail.com>
2023-04-05 20:15:24 +0900

    schema.resolvers.go: remove ConnectDB

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -10,8 +10,6 @@ import (
 	"math/rand"
 
 	"github.com/ghsable/graphql-playground.go/graph/model"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 // createTodo mutation
@@ -71,18 +69,3 @@ func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func ConnectDB() *gorm.DB {
-	dsn := "dev.db"
-	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-	return db
-}
